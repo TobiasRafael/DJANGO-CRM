09558bec2e96f607afa6ef4f329becbce6dfb654
TobiasRafael <rafaelmontas96@gmail.com>
2023-12-09 16:51:47 -0400

    delete, add forms/views added

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register')
+    path('register/', views.register_user, name='register'),
+    path('customer/<int:pk>', views.customer_record, name='customer'),
+    path('delete_customer/<int:pk>', views.delete_customer, name='delete_customer'),
+    path('add_customer/', views.add_customer, name='add_customer')
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddCustomerForm
 from .models import Customer
 
 # Create your views here.
@@ -50,3 +50,35 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
 
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Customer.objects.get(id=pk)
+        return render(request, 'customer.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You must be logged in to view that page')
+        return redirect('home')
+    
+def delete_customer(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Customer.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Records Deleted Succesfully")
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to do that')
+        return redirect('home')
+
+def add_customer(request):
+    form  = AddCustomerForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_customer = form.save()
+                messages.success(request, "Customer Added")
+                return redirect('home')
+        return render(request, 'add_customer.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
+
